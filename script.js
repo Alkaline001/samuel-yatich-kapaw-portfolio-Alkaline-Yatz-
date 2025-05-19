c7390d76f7158d4c195df6699ed34a1110d970b0
Alkaline Yatz <yyatich@gmail.com>
2025-05-19 22:48:48 +0300

    Updated portfolio website with responsive design and fixed tab functionality

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hamburger menu
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navUl = document.querySelector("nav ul");
+    if (menuToggle && navUl) {
+        menuToggle.addEventListener("click", () => {
+            navUl.classList.toggle("active");
+        });
+    }
 
-    var tablinks=document.getElementsByClassName("tab-links");
-    var tabcontents=document.getElementsByClassName("tab-contents");
-    function opentab(tabname){
-        for(tablink of tablinks){
-            tablink.classList.remove("active-link");
+    // Tab switching
+    const tabLinks = document.getElementsByClassName("tab-links");
+    const tabContents = document.getElementsByClassName("tab-content");
+
+    function openTab(tabName) {
+        for (let tabLink of tabLinks) {
+            tabLink.classList.remove("active-link");
+            tabLink.setAttribute("aria-selected", "false");
         }
-        for(tabcontent of tabcontents){
-            tabcontent.classList.remove("active-tab");
+        for (let tabContent of tabContents) {
+            tabContent.classList.remove("active-tab");
+            tabContent.setAttribute("aria-hidden", "true");
         }
         event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
+        event.currentTarget.setAttribute("aria-selected", "true");
+        document.getElementById(tabName).classList.add("active-tab");
+        document.getElementById(tabName).setAttribute("aria-hidden", "false");
     }
+
+    for (let tabLink of tabLinks) {
+        tabLink.addEventListener("click", function (event) {
+            openTab(this.dataset.tab);
+        });
+    }
+});
